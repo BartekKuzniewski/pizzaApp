bde8a75ac44a9a7a0a296a6a3bacdce61547b0d8
Bartek <kuzniewski.bar@gmail.com>
2022-03-10 16:38:20 +0100

    added variables in main.js

---
+++ src/js/main.js
@@ -0,0 +1,18 @@
+const firstPizzaBox = document.querySelector('.first-box');
+const secondPizzaBox = document.querySelector('.second-box');
+
+const firstDiameter = firstPizzaBox.querySelector('#first-diameter');
+const firstAmount = firstPizzaBox.querySelector('#first-amount');
+const firstPrice = firstPizzaBox.querySelector('#first-price');
+
+const secondDiameter = secondPizzaBox.querySelector('#second-diameter');
+const secondAmount = secondPizzaBox.querySelector('#second-amount');
+const secondPrice = secondPizzaBox.querySelector('#second-price');
+
+const firstArea = document.querySelector('.first-area');
+const firstCost = document.querySelector('.first-cost');
+
+const secondArea = document.querySelector('.second-area');
+const secondCost = document.querySelector('.second-cost');
+
+const chooseThisPizza = document.querySelector('.choose-this');
